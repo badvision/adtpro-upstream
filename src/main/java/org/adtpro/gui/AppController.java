a6f40c8bb32b33c496d8b66fd2c50184eddfb394
badvision <brendan.robert@gmail.com>
2016-10-31 02:03:20 -0500

    Starting point for new JavaFX user interface

---
+++ src/main/java/org/adtpro/gui/AppController.java
@@ -0,0 +1,25 @@
+ Error reading included file Templates/javafx/Templates/Licenses/license-gpl2.txt
+package org.adtpro.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author blurry
+ */
+
+
+public class AppController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
